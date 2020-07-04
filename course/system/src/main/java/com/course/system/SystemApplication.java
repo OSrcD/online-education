f9d44d3c82076c1cbf6dc4b9d28c8f11e694256e
OpenSrcDeveloper <OpenDeveloper@mywiz.cn>
2020-07-04 18:07:13 +0800

    04-搭建业务模块-system

--- course/system/src/main/java/com/course/system/SystemApplication.java
+++ course/system/src/main/java/com/course/system/SystemApplication.java
@@ -1,27 +1,27 @@
-package com.course;
+package com.course.system;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-@EnableEurekaServer
-public class EurekaApplication {
+@EnableEurekaClient
+public class SystemApplication {
 
-    private static Logger Log = LoggerFactory.getLogger(EurekaApplication.class);
+    private static Logger Log = LoggerFactory.getLogger(SystemApplication.class);
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication(EurekaApplication.class);
+        SpringApplication app = new SpringApplication(SystemApplication.class);
 
         Environment environment = app.run(args).getEnvironment();
 
 
         Log.info("应用启动成功！！");
 
-        Log.info("Eureka的地址是：\thttp://127.0.0.1:{}",environment.getProperty("server.port"));
+        Log.info("system的地址是：\thttp://127.0.0.1:{}",environment.getProperty("server.port"));
 
     }
 
