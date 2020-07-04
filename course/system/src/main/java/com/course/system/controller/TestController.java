cf41ec67c438bee0dbc5ec89f78cdc642d9af9a5
OpenSrcDeveloper <OpenDeveloper@mywiz.cn>
2020-07-04 20:05:24 +0800

    05-搭建路由模块-gateway

--- course/system/src/main/java/com/course/system/controller/TestController.java
+++ course/system/src/main/java/com/course/system/controller/TestController.java
@@ -1,13 +1,15 @@
 package com.course.system.controller;
 
-import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+
 public class TestController {
 
-    @GetMapping("/test")
+    @RequestMapping("/test")
     public String test(){
-        return "suceess";
+        return "success";
     }
 }
