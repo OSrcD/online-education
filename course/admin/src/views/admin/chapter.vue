769b828bb93a821be1f07dbd481da4cc056831fa
OpenSrcDeveloper <OpenDeveloper@mywiz.cn>
2020-07-13 19:32:07 +0800

    19-增加新增大章功能

--- course/admin/src/views/admin/chapter.vue
+++ course/admin/src/views/admin/chapter.vue
@@ -1,11 +1,18 @@
 <template>
   <div>
     <p>
+      <button v-on:click="add()" class="btn btn-white btn-default btn-round">
+        <i class="ace-icon fa fa-edit"></i>
+        新增
+      </button>
+      &nbsp;
       <button v-on:click="list(1)" class="btn btn-white btn-default btn-round">
         <i class="ace-icon fa fa-refresh"></i>
         刷新
       </button>
     </p>
+
+
 
     <pagination ref="pagination" v-bind:list="list" v-bind:itemCount="8"></pagination>
 
@@ -83,6 +90,40 @@
 
       </tbody>
     </table>
+
+    <div id="form-modal" class="modal fade" tabindex="-1" role="dialog">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content">
+          <div class="modal-header">
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+            <h4 class="modal-title">表单</h4>
+          </div>
+          <div class="modal-body">
+            <form class="form-horizontal">
+                <div class="form-group">
+                  <label class="col-sm-2 control-label">名称</label>
+                  <div class="col-sm-10">
+                    <input v-model="chapter.name" class="form-control" placeholder="名称">
+                  </div>
+                </div>
+
+                <div class="form-group">
+                  <label class="col-sm-2 control-label">课程ID</label>
+                  <div class="col-sm-10">
+                    <input v-model="chapter.courseId"  class="form-control" placeholder="课程ID">
+                  </div>
+                </div>
+
+            </form>
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-default" data-dismiss="modal">取消</button>
+            <button v-on:click="save()" type="button" class="btn btn-primary">保存</button>
+          </div>
+        </div><!-- /.modal-content -->
+      </div><!-- /.modal-dialog -->
+    </div><!-- /.modal -->
+
   </div>
 
 </template>
@@ -96,6 +137,7 @@
     name: 'chapter',
     data: function(){
       return {
+        chapter: {},
         chapters: []
       }
     },
@@ -106,6 +148,11 @@
       // this.$parent.activeSidebar("business-chapter-sidebar");
     },
     methods: {
+      add(){
+        let _this = this;
+        $("#form-modal").modal("show");
+      },
+
       list(page){
         let _this = this;
 
@@ -114,9 +161,22 @@
           size: _this.$refs.pagination.size,
         }).then((response)=>{
           console.log("查询大章列表结果：",response);
-          _this.chapters = response.data.list;
-          _this.$refs.pagination.render(page,response.data.total);
+          let resp = response.data;
+          _this.chapters = resp.content.list;
+          _this.$refs.pagination.render(page,resp.content.total);
         })
+      },
+      save(page){
+        let _this = this;
+        _this.$ajax.post("http://127.0.0.1:9000/business/admin/chapter/save",
+        _this.chapter).then((response)=>{
+          console.log("保存大章列表结果：",response);
+          let resp = response.data;
+          if(resp.success){
+            $("#form-modal").modal("hide");
+            _this.list(1);
+          }
+        });
       }
     }
   }
